027143f0b53e4345c1955fe3fad3ab2d072b20ae
aniket sharma <aniket965.as@gmail.com>
2017-02-22 18:13:46 +0530

    hi

--- script/script.js
+++ script/script.js
@@ -17,7 +17,6 @@ function draw() {
     var fontpoints = font.textToPoints('Aniket965',20,20);
   for(var i =0 ;i < fontpoints.length;i++){
    stroke(mouseX,random(0,255),mouseY);
-   console.log('hi');
    strokeWeight(2);
    ellipse(fontpoints[i].x,fontpoints[i].y,random(1,9),random(1,9));
    rect(fontpoints[i].x,fontpoints[i].y,random(1,2),random(1,2));
