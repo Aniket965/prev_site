eadd3a6a944fdc7d69fa73d20b63b076204bdfa0
aniket sharma <aniket965.as@gmail.com>
2017-02-22 18:58:21 +0530

    changed

--- script/script.js
+++ script/script.js
@@ -2,21 +2,20 @@
   font = loadFont("assets/lato.ttf");
 }
 function setup() {
-var can =createCanvas(1200,500);
-  can.parent('aniBox');
+
   }
 
 
 
 function draw() {
-  
-  background(255);
-   textSize(200);
-    fill(255);
+var can =createCanvas(1800,500);
+  can.parent('aniBox');
+  textSize(200);
+  fill(255);
    strokeWeight(4);
-    var fontpoints = font.textToPoints('Aniket965',20,20);
+    var fontpoints = font.textToPoints('Aniket965',250,200);
   for(var i =0 ;i < fontpoints.length;i++){
-   stroke(mouseX,random(0,255),mouseY);
+   stroke(mouseX/200,random(0,255),mouseY);
    strokeWeight(2);
    ellipse(fontpoints[i].x,fontpoints[i].y,random(1,9),random(1,9));
    rect(fontpoints[i].x,fontpoints[i].y,random(1,2),random(1,2));
