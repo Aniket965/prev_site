1e1b91a3c4654c891375e7db0d7a05f9ae7b7b3c
aniket sharma <aniket965.as@gmail.com>
2017-02-22 17:57:10 +0530

    added something

--- script/script.js
+++ script/script.js
@@ -1,4 +1,29 @@
 $('document').ready(function(){
-   
+   $('.can').ready(function(){
+       function preload(){
+  font = loadFont("assets/lato.ttf");
+}
+function setup() {
+ 
+ 
+  }
+
+
+
+function draw() {
+  createCanvas(1200,500);
+  background(255);
+   textSize(200);
+    fill(255);
+   strokeWeight(4);
+    var fontpoints = font.textToPoints('Aniket965',200,200);
+  for(var i =0 ;i < fontpoints.length;i++){
+   stroke(mouseX,random(0,255),mouseY);
+   strokeWeight(2);
+   ellipse(fontpoints[i].x,fontpoints[i].y,random(1,9),random(1,9));
+   rect(fontpoints[i].x,fontpoints[i].y,random(1,2),random(1,2));
+ }
+}
+   })
 });
 
