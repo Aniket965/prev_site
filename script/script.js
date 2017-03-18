2e26a3f35d219b154982db4cb07fd0f3efa7a33b
aniket965 <aniket965.as@gmail.com>
2017-03-19 00:53:35 +0530

    added links

--- script/script.js
+++ script/script.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
-    // alert('hi');
+    
+    
+    // SocialLinks
+    $('#github').click(function(){
+       window.open("https://github.com/aniket965","github") 
+    });
+    $('#facebook').click(function(){
+       window.open("https://www.facebook.com/aniket.sharma.583671","facebook") 
+    });
+    $('#insta').click(function(){
+       window.open("https://www.instagram.com/aniket965.as/","instagram") 
+    });
+    $('#youtube').click(function(){
+       window.open("https://github.com/aniket965","github") 
+    });
+    $('#googleplus').click(function(){
+       window.open("https://github.com/aniket965","github") 
+    });
+    $('#twitter').click(function(){
+       window.open("https://twitter.com/aniket965as","twitter") 
+    });
+    $('#linkedin').click(function(){
+       window.open("https://github.com/aniket965","linkedin");
+    });
+
+
+
+
 
 });
