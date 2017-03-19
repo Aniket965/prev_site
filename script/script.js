3b4142e00f9d03e0ba827c60d24718a322be3d30
aniket sharma <aniket965.as@gmail.com>
2017-03-19 17:25:11 +0530

    completed links

--- script/script.js
+++ script/script.js
@@ -12,19 +12,17 @@ $(document).ready(function(){
        window.open("https://www.instagram.com/aniket965.as/","instagram") 
     });
     $('#youtube').click(function(){
-       window.open("https://github.com/aniket965","github") 
+       window.open("https://www.youtube.com/channel/UCdDzhOvIjJ8CfrZ7AY_y59g","youtube") 
     });
     $('#googleplus').click(function(){
-       window.open("https://github.com/aniket965","github") 
+       window.open("https://plus.google.com/u/0/105401357309088653716","googleplus") 
     });
     $('#twitter').click(function(){
        window.open("https://twitter.com/aniket965as","twitter") 
     });
     $('#linkedin').click(function(){
-       window.open("https://github.com/aniket965","linkedin");
+       window.open("https://www.linkedin.com/in/aniket-sharma-548413131/","linkedin");
     });
-
-
 
 
 
